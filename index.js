ba150182da4b86ddae23e8945fe96ddea89b65b4
Broc Harcourt & Pearl Robin <pair+broc-harcourt.pearlr@devbootcamp.com>
2016-05-23 11:04:49 +1200

    render result of iss api call

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
+var whereTheIss = require('./views/whereTheIss.hbs')
 
 xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
   if (err) console.log(err) // do something
 
   console.log(data.body)
-  document.body.innerHTML = example({ name: "Space" });
-})
+  document.body.innerHTML = example({ name: "Space" })
 
+  var satellite = JSON.parse(data.body.replace('/[|]/g', ''))[0]
+
+  xhr.get('https://api.wheretheiss.at/v1/satellites/' + satellite.id, function(err, res) {
+    console.log((err) ? err : res)
+
+    var iss = JSON.parse(res.body)
+
+    document.body.innerHTML = whereTheIss(iss)
+
+  })
+
+})
