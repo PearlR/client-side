f769d250c494925ce0289f3d51f8062744b3c6b8
Broc Harcourt & Pearl Robin <pair+broc-harcourt.pearlr@devbootcamp.com>
2016-05-23 11:03:07 +1200

    Updates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
+var whereTheIss = require('./views/whereTheIss.hbs')
 
 xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
   if (err) console.log(err) // do something
@@ -7,8 +8,7 @@ xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
   console.log(data.body)
   document.body.innerHTML = example({ name: "Space" })
 
-  var satellite = JSON.parse(data.body.replace('/[|]/g', ''))
-  console.log(satellite)
+  var satellite = JSON.parse(data.body.replace('/[|]/g', ''))[0]
 
   xhr.get('https://api.wheretheiss.at/v1/satellites/' + satellite.id, function(err, res) {
     console.log((err) ? err : res)
