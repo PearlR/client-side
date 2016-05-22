d76e5ee4404389dfdb2a55840982626316afed45
Broc Harcourt & Pearl Robin <pair+broc-harcourt.pearlr@devbootcamp.com>
2016-05-23 11:00:11 +1200

    Updates

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
   if (err) console.log(err) // do something
 
   console.log(data.body)
-  console.log(data.body[0])
-  console.log(data.body[0].id)
-  document.body.innerHTML = example({ name: "Space" });
+  document.body.innerHTML = example({ name: "Space" })
 
-  xhr.get('https://api.wheretheiss.at/v1/satellites/' + JSON.parse(data.body.replace('/[|]/g', ''))[0].id, function(err, iss) {
-    console.log((err) ? err : iss)
+  var satellite = JSON.parse(data.body.replace('/[|]/g', ''))
+
+  xhr.get('https://api.wheretheiss.at/v1/satellites/' + satellite.id, function(err, res) {
+    console.log((err) ? err : res)
+
+    var iss = JSON.parse(res.body)
+
+    document.body.innerHTML = whereTheIss(iss)
+
   })
 
 })
-
